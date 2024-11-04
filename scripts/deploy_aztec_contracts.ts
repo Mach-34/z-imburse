1de223ee679f6193d034ae15c02afecfebe705f6
Ian-Bright <ianbrighton1@gmail.com>
2024-11-04 19:02:04 +0700

    feat: create acocunt works when driven from ts with package.json set to module

--- scripts/deploy_aztec_contracts.ts
+++ scripts/deploy_aztec_contracts.ts
@@ -1,33 +1,41 @@
-import { createPXEClient, waitForPXE } from "@aztec/aztec.js";
-import {
-    createAccounts,
-} from "@aztec/accounts/testing";
-import { TokenContract } from "@aztec/noir-contracts.js";
+import { createPXEClient, Fq, Fr, waitForPXE } from "@aztec/aztec.js";
+import { createAccount } from "@aztec/accounts/testing";
+import { getSchnorrAccount } from "@aztec/accounts/schnorr";
+import { setup } from "../tests/utils";
 
 
 const DEFAULT_PXE_URL = "http://localhost:8080";
 
 const deploy = async () => {
-    const pxe = createPXEClient(DEFAULT_PXE_URL);
+    const pxe = await createPXEClient("http://localhost:8080");
     await waitForPXE(pxe);
-    const accounts = await createAccounts(pxe, 4);
 
-    // deploy USDC contract
-    const usdc = await TokenContract.deploy(
-        accounts[0],
-        accounts[0].getAddress(),
-        "USDC",
-        "USD Token",
-        6n
-    )
-        .send()
-        .deployed();
+    const schnorr = getSchnorrAccount(
+        pxe,
+        Fr.fromString('0x06ef861b5853f12549a8d7e3e67083ae680123dcbb12cedb6d3060075f9d0b3c'),
+        Fq.fromString('0x082e6d118b06b9fb3bf5bcaa5328f6f742c86dad8df04d5601c3508c827d3c38'),
+        0
+    );
 
-    console.log('Account #1: ', accounts[0].getAddress().toString());
-    console.log('Account #2: ', accounts[1].getAddress().toString());
-    console.log('Account #3: ', accounts[2].getAddress().toString());
-    console.log('Account #4: ', accounts[3].getAddress().toString());
-    console.log('USDC Contract: ', usdc.address.toString())
+    // check if account is already registerd on pxe
+    const isRegistered = await pxe.getRegisteredAccount(
+        schnorr.getAddress()
+    );
+
+    // if account not already registered then deploy to pxe
+    if (!isRegistered) {
+        await schnorr.deploy().wait();
+    }
+
+    const superuser = await schnorr.getWallet()
+    const escrowAdmin = await createAccount(pxe);
+    const alice = await createAccount(pxe);
+    const bob = await createAccount(pxe);
+
+    const { usdc, registry } = await setup(superuser, [escrowAdmin]);
+
+    // console.log('USDC contract: ', usdc.address.toString())
+    // console.log('Registry contract: ', registry.address.toString())
 }
 
 deploy();

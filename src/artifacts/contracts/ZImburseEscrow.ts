c1af77a232b4509810447fac26616524862570c1
Jack Gilcrest <me@jp4g.llc>
2024-11-09 07:34:33 -0700

    can run txe in correct version now

--- src/artifacts/contracts/ZImburseEscrow.ts
+++ src/artifacts/contracts/ZImburseEscrow.ts
@@ -46,6 +46,11 @@ import ZImburseEscrowContractArtifactJson from './ZImburseEscrow.json' assert { 
 //@ts-ignore
 //@ts-ignore
 //@ts-ignore
+//@ts-ignore
+//@ts-ignore
+//@ts-ignore
+//@ts-ignore
+//@ts-ignore
 export const ZImburseEscrowContractArtifact = loadContractArtifact(ZImburseEscrowContractArtifactJson as NoirCompiledContract);
 
 
